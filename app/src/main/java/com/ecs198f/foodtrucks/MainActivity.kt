33d9d06283739dacfe82fc5379152f9c04825d16
Vishal K <vishalksai@gmail.com>
2022-04-29 14:06:17 -0700

    init commit

---
+++ app/src/main/java/com/ecs198f/foodtrucks/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ecs198f.foodtrucks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ecs198f.foodtrucks.databinding.ActivityMainBinding
+import java.time.LocalDateTime
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
